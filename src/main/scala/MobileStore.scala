19674463cb30dd76e84ac5000bd81547c33cb2f5
jitendra2329 <jitendra.kumar@nashtechglobal.com>
2024-02-21 16:29:12 +0530

    implemented the handlling post endpoind and post endpoint with query parameters

--- src/main/scala/MobileStore.scala
+++ src/main/scala/MobileStore.scala
@@ -1,45 +1,46 @@
-
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest, HttpResponse, StatusCodes, Uri}
+import akka.http.scaladsl.model._
 import akka.pattern.ask
 import akka.util.Timeout
-
-import scala.concurrent.duration._
-import scala.concurrent.Future
 import spray.json._
 
+import scala.concurrent.Future
+import scala.concurrent.duration._
 import scala.io.StdIn
 import scala.language.postfixOps
+import scala.util.{Failure, Success, Try}
 
 case class Mobile(name: String, model: String)
 
+case class MobileResponse(name: String, model: String, id: Int)
+
 object MobileDb {
-  case class createMobile(mobile: Mobile)
+  case class CreateMobile(mobile: Mobile)
 
-  case class createdMobile(id: Int)
+  case class CreatedMobile(id: Int)
 
-  case object getAllMobiles
+  case object GetAllMobiles
 
-  case class findMobile(id: Int)
+  case class GetMobileById(id: Int)
 }
 
 class MobileDb extends Actor with ActorLogging {
 
   import MobileDb._
 
-  var mobiles: Map[Int, Mobile] = Map.empty
-  var currentMobileId = 0
+  private var mobiles: Map[Int, Mobile] = Map.empty
+  private var currentMobileId = 0
 
   override def receive: Receive = {
-    case createMobile(mobile) =>
+    case CreateMobile(mobile) =>
       mobiles = mobiles + (currentMobileId -> mobile)
-      sender() ! createdMobile(currentMobileId)
+      sender() ! CreatedMobile(currentMobileId)
       currentMobileId = currentMobileId + 1
-    case getAllMobiles =>
+    case GetAllMobiles =>
       log.info("Searching for all mobiles!")
       sender() ! mobiles.values.toList
-    case findMobile(id) =>
+    case GetMobileById(id) =>
       log.info(s"Searching mobile by id: $id!")
       sender() ! mobiles.get(id)
   }
@@ -50,48 +51,115 @@ trait MobileFormatProtocol extends DefaultJsonProtocol {
 }
 
 object MobileStore extends App with MobileFormatProtocol {
+
   import MobileDb._
 
   implicit val system: ActorSystem = ActorSystem("actorSystem")
   import system.dispatcher
 
-  val mobileDbActor = system.actorOf(Props[MobileDb], "mobiledb")
-  val listOfMobiles = List(
+  private val mobileDbActor = system.actorOf(Props[MobileDb], "mobiledb")
+  private val listOfMobiles = List(
     Mobile("SAMSUNG", "M13"),
     Mobile("MOTO", "G84"),
   )
 
-  listOfMobiles.foreach{ m =>
-    mobileDbActor ! createMobile(m)
+  listOfMobiles.foreach { m =>
+    mobileDbActor ! CreateMobile(m)
   }
 
   implicit val defaultTimeout: Timeout = Timeout(2 seconds)
 
-  private val httpRequestHandler: HttpRequest=> Future[HttpResponse] = {
-    case HttpRequest(HttpMethods.GET, Uri.Path("/api/mobile"), _, _, _) =>
-      val allMobiles = (mobileDbActor ? getAllMobiles).mapTo[List[Mobile]]
-      allMobiles.map { mob =>
-        HttpResponse(
+  private def getMobile(query: Uri.Query): Future[HttpResponse] = {
+    val mobileId = query.get("id")
+
+    mobileId match {
+      case Some(id) => Try(id.toInt) match {
+        case Failure(_) => Future(HttpResponse(StatusCodes.NoContent))
+        case Success(value) =>
+          val mobileFuture = (mobileDbActor ? GetMobileById(value)).mapTo[Option[Mobile]]
+          mobileFuture.map {
+            case Some(value) => HttpResponse(
+              StatusCodes.OK,
+              entity = HttpEntity(
+                ContentTypes.`application/json`,
+                value.toJson.prettyPrint
+              )
+            )
+            case None => HttpResponse(StatusCodes.NoContent)
+          }
+      }
+      case None => Future(HttpResponse(StatusCodes.NoContent))
+    }
+  }
+
+
+  private val httpRequestHandler: HttpRequest => Future[HttpResponse] = {
+    case HttpRequest(HttpMethods.GET, uri@Uri.Path("/api/mobile"), _, _, _) =>
+      val query = uri.query()
+      if (query.isEmpty) {
+        val allMobiles = (mobileDbActor ? GetAllMobiles).mapTo[List[Mobile]]
+
+        for {
+          mob <- allMobiles
+        } yield HttpResponse(
           StatusCodes.OK,
           entity = HttpEntity(
             ContentTypes.`application/json`,
             mob.toJson.prettyPrint
           )
         )
+      } else {
+        getMobile(query)
+      }
+    case HttpRequest(HttpMethods.POST, Uri.Path("/api/mobile"), _, entity, _) =>
+
+      val strictFuture = entity.toStrict(2 seconds)
+      strictFuture.flatMap { strictEntity =>
+        val mobileJsonString = strictEntity.data.utf8String
+        println(s"Received data from client: $mobileJsonString")
+        val mobile = mobileJsonString.parseJson.convertTo[Mobile]
+        val mobileCreated = (mobileDbActor ? CreateMobile(mobile)).mapTo[CreatedMobile]
+        for {
+          mob <- mobileCreated
+        } yield {
+          HttpResponse(
+            StatusCodes.OK,
+            entity = HttpEntity(
+              ContentTypes.`text/plain(UTF-8)`,
+              mob.id.toString
+            )
+          )
+        }
       }
 
     case request: HttpRequest =>
-              request.discardEntityBytes()
-              Future{
-                HttpResponse(status = StatusCodes.OK)
-              }
-//    case HttpRequest(HttpMethods.POST, Uri.Path("/api/mobile"), _, _, _) =>
-//      Future(HttpResponse(
-//        StatusCodes.OK,
-//      ))
+      request.discardEntityBytes()
+      Future {
+        HttpResponse(status = StatusCodes.OK)
+      }
+
   }
 
-  // converting to json OR marshaling
+  private val futureRequest = Http().newServerAt("localhost", 8090).bind(httpRequestHandler)
+
+  println(s"Server online at http://localhost:8090/")
+  StdIn.readLine()
+
+  futureRequest
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
+}
+
+// THE OTHER WAY TO CREATE RESPONSE ON THE BASIS OF THE ENTITY BODY
+//      entity.dataBytes.runFold(ByteString(""))(_ ++ _).map { body =>
+//        val requestData = body.utf8String
+//        val caseClassMobile = requestData.parseJson.convertTo[Mobile]
+//        mobileDbActor ! CreateMobile(caseClassMobile)
+//        println(s"Received data from client: $requestData")
+//        HttpResponse(StatusCodes.OK)
+//      }
+
+// converting to json OR MARSHALLING
 //  val simpleMobile = Mobile("SAMSUNG", "M13")
 //  val mobileJson = simpleMobile.toJson
 //
@@ -104,16 +172,5 @@ object MobileStore extends App with MobileFormatProtocol {
 //      |}
 //      |""".stripMargin
 
-  // converting to case class OR unmarshalling
+// converting to case class OR UNMARSHALLING
 //  val mobileCaseClass = mobileStringJson.parseJson.convertTo[Mobile]
-
-  private val futureRequest = Http().newServerAt("localhost", 8090).bind(httpRequestHandler)
-
-  println(s"Server online at http://localhost:8090/")
-  StdIn.readLine()
-
-  futureRequest
-    .flatMap(_.unbind())
-    .onComplete(_ => system.terminate())
-
-}
